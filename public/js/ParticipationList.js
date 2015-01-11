eb3feaac172f0ce1c1ae7fc2116baf824c5ce21a
Xelvair <marv995@web.de>
2015-01-11 17:55:38 +0100

    fixes

--- public/js/ParticipationList.js
+++ public/js/ParticipationList.js
@@ -2,9 +2,7 @@
 	$(document).ready(function(){
 		$(".participation_entry").on("click", ".rights", function(e){
 			e.stopPropagation();
-			console.log("dblfgt");
-			console.log(e);
-			var rights_rolldown = $(e.delegateTarget).find(".participation_entry_rights_rolldown").get();
+			var rights_rolldown = $(e.delegateTarget).find(".participation_entry_rights_rolldown");
 
 			$(rights_rolldown).slideToggle(250);
 		});
